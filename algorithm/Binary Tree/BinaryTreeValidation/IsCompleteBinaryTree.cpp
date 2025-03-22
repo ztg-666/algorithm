1b82eb61b4fd8d2cc13d9fb0d5ff4327aee39e6a
ztg-666 <20240423022@bucm.edu.cn>
2025-03-22 15:40:52 +0800

    Update IsCompleteBinaryTree.cpp

--- algorithm/Binary Tree/BinaryTreeValidation/IsCompleteBinaryTree.cpp
+++ algorithm/Binary Tree/BinaryTreeValidation/IsCompleteBinaryTree.cpp
@@ -50,3 +50,93 @@ bool isCBT(Node* head) {
     // 如果遍历完整棵树没有违反上述条件，则返回 true
     return true;
 }
+
+
+
+// 定义二叉树节点类
+class TreeNode {
+public:
+    int val;          // 节点的值
+    TreeNode* left;   // 指向左子节点的指针
+    TreeNode* right;  // 指向右子节点的指针
+
+    // 构造函数，初始化节点的值，并将左右子节点设为nullptr
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+// 定义判断二叉树是否为完全二叉树的类
+class CompletenessOfBinaryTree {
+public:
+    // 如果测试数据量变大了就修改这个值
+    static const int MAXN = 101;
+
+    // 判断给定的二叉树是否为完全二叉树
+    static bool isCompleteTree(TreeNode* h) {
+        // 如果根节点为空，则树为空，空树被认为是完全二叉树
+        if (h == nullptr) {
+            return true;
+        }
+
+        // 使用队列进行层次遍历
+        std::queue<TreeNode*> queue;
+        queue.push(h);
+
+        // 标记是否遇到过左右子节点不双全的节点
+        bool leaf = false;
+
+        // 开始层次遍历
+        while (!queue.empty()) {
+            // 取出队列中的第一个节点
+            h = queue.front();
+            queue.pop();
+
+            // 如果当前节点左子节点为空但右子节点不为空，则不是完全二叉树
+            // 或者如果已经遇到过左右子节点不双全的节点，但当前节点还有子节点，则不是完全二叉树
+            if ((h->left == nullptr && h->right != nullptr) || (leaf && (h->left != nullptr || h->right != nullptr))) {
+                return false;
+            }
+
+            // 如果左子节点不为空，将其加入队列
+            if (h->left != nullptr) {
+                queue.push(h->left);
+            }
+
+            // 如果右子节点不为空，将其加入队列
+            if (h->right != nullptr) {
+                queue.push(h->right);
+            }
+
+            // 如果当前节点左子节点或右子节点为空，则标记leaf为true
+            if (h->left == nullptr || h->right == nullptr) {
+                leaf = true;
+            }
+        }
+
+        // 如果遍历完整棵树没有违反完全二叉树的条件，则返回true
+        return true;
+    }
+};
+
+void freeTree(TreeNode* root) {
+    if (!root) return;
+    freeTree(root->left);
+    freeTree(root->right);
+    delete root;
+}
+
+int main() {
+    // 创建一个完全二叉树
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+    root->right->left = new TreeNode(6);
+
+    // 检查是否为完全二叉树
+    cout << (CompletenessOfBinaryTree::isCompleteTree(root) ? "Complete" : "Not complete") << endl;
+
+    freeTree(root);
+
+    return 0;
+}
